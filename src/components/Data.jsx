8c19e197d38ae23370ca944c50c9acde36a61984
Abhinav Kumar <abhinavkumar50362@proton.me>
2024-06-10 14:29:17 +0000

    Component Added ➕

---
+++ src/components/Data.jsx
@@ -0,0 +1,43 @@
+import education from '../assets/education.svg';
+import location from '../assets/location.svg';
+import college from '../assets/college.svg';
+
+const profile_data = {
+    name: 'Abhinav Kumar',
+    about: {
+        location: {
+            about: 'Lucknow (India)',
+            img: location
+        },
+        college: {
+            about: 'SRMCEM',
+            img: college
+        },
+        education: {
+            about: 'B.Tech CSE(AIML) 2026',
+            img: education
+        },
+        
+    },
+    image:"https://ugc.production.linktr.ee/3eba07f1-4bc1-4bf7-9e86-5e4f2fb9764d_20231108-084516-1.jpeg?io=true&size=avatar-v3_0",
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Node', 'MongoDB']
+}
+
+const links_data = {
+    "facebook": "https://facebook.com/abhinavkumar2369",
+    "instagram": "https://instagram.com/abhinavkumar2369",
+    "thread":"https://www.threads.net/@abhinavkumar2369",
+    "snapchat": "https://snapchat.com/add/abhinavkumar2369",
+    "linkedIn": "https://linkedin.com/in/abhinavkumar2369",
+    "mail": "mailto:abhinavkumar2369@proton.me",
+    "telegram": "https://t.me/abhinavkumar2369",
+    "whatsapp": "https://wa.me/919455602369",
+    "discord": "https://discord.com/users/abhinavkumar2369",
+}
+
+const other_links_data = {
+    "Portpolio Website": "https://abhinavkumarofficial.com",
+    "Github": "https://github/com/abhinavkumar2369",
+}
+
+export {profile_data, links_data, other_links_data};
